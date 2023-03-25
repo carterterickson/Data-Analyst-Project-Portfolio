6da8455fbe8570dd41bcd81117915b63dbf2942d
Carter Erickson <79028207+carterterickson@users.noreply.github.com>
2023-03-24 23:23:53 -0600

    Add files via upload

---
+++ DHFR Classification Deploy.R
@@ -0,0 +1,55 @@
+### Importing Libraries ###
+library(datasets)
+library(caret)
+
+# Importing dhfr dataset
+data(dhfr)
+
+# Check for missing data
+sum(is.na(dhfr))
+
+# Set seed to make sure model is reproducible
+set.seed(888)
+
+# Perform stratified random split of data
+TrainingIndex = createDataPartition(dhfr$Y, p=0.8, list = FALSE)
+TrainingSet = dhfr[TrainingIndex,] # Training set
+TestingSet = dhfr[-TrainingIndex,] # Test set
+
+
+### SVM Model (polynomial kernel) ###
+
+# Build Training Model
+Model = train(Y ~ ., data = TrainingSet, 
+              method = "svmPoly",
+              na.action = na.omit,
+              preProcess=c("scale", "center"),
+              trControl = trainControl(method = "none"),
+              tuneGrid = data.frame(degree=1, scale = 1, C = 1)
+)
+
+# Save model to RDS file
+
+saveRDS(Model, "Model.rds")
+
+# Read the model from RDS file
+
+read.Model = readRDS("Model.rds")
+
+# Apply model for prediction
+Model.training = predict(read.Model, TrainingSet) # Apply model to make prediction on Training set
+Model.testing = predict(read.Model, TestingSet) # Apply model to make prediction on Testing set
+
+# Model performance
+Model.training.confusion = confusionMatrix(Model.training, TrainingSet$Y)
+Model.testing.confusion = confusionMatrix(Model.testing, TestingSet$Y)
+
+print(Model.training.confusion)
+print(Model.testing.confusion)
+
+# Feature importance
+Importance = varImp(Model)
+plot(Importance, top = 25)
+
+
+
